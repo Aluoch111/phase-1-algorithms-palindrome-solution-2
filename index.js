1788b2dd685558df414b3c272cbed9271de00f1c
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-09-07 18:07:28 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word.length <= 1){
+    return true;
+
+  }else if(word[0] == word[word.length - 1]){
+    return isPalindrome(word.substring(1, word.length - 1));
+  }
+
+  return false;
 }
+
+
 
 /* 
   Add your pseudocode here
+    check the size of the string
+  if first and last characters match...remove elements
 */
 
 /*
   Add written explanation of your solution here
+  When the string is the same length or less than or equal to 1
+  return true
 */
 
 // You can run `node index.js` to view these console logs
